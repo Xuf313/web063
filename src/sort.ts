bb131c2265cb37f83ba9f0c0f06beef62cf26d1d
s202455461 <htetzayar313@gmail.com>
2025-09-17 17:46:38 +0900

    updated

--- src/sort.ts
+++ src/sort.ts
@@ -1,12 +1,16 @@
+function compare(a: number, b: number, order: "asc" | "desc"): boolean {
+    return order === "asc" ? a > b : a < b;
+}
+
 export function simpleSort(arr: number[], order : "asc" | "desc" = "asc"): number[] {
     const temp = [...arr];
     let n = temp.length;
     for (let i = 0; i < n - 1; i++) {
         for (let j = 0; j < n - 1 - i; j++) {
             if (
-                temp[j + 1] !== undefined &&
-                temp[j] !== undefined &&
-                (order === "asc" ? temp[j]! > temp[j + 1]! : temp[j]! < temp[j + 1]!)
+                typeof temp[j] === "number" &&
+                typeof temp[j + 1] === "number" &&
+                compare(temp[j] as number, temp[j + 1] as number, order)
             ) {
                 [temp[j]!, temp[j + 1]!] = [temp[j + 1]!, temp[j]!];
             }
